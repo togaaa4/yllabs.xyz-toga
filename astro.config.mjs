d45f0b15ec338dd662dde62c4ebb15c2dc46b2f1
Toga 3 <they.luv.lunaaa4@gmail.com>
2025-11-13 19:06:30 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 import netlifyEdge from '@astrojs/netlify-edge';
+import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
-  output: 'server', // edge server build
-  adapter: netlifyEdge(),
+  output: 'server',       // edge server build
+  adapter: netlifyEdge(), // Netlify Edge adapter
+  integrations: [tailwind()],
 });
