d30c4ce6c8a584e12f23d2d1bf748ae56c1464c6
Toga 3 <they.luv.lunaaa4@gmail.com>
2025-11-14 08:10:28 +0000

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify";
+import react from "@astrojs/react"; // React integration
 
 export default defineConfig({
   output: "static",
   adapter: netlify(),
   site: "https://toga.yllabs.xyz",
   base: "/",
-  integrations: [],
+  integrations: [react], // Enable React
   vite: {
     optimizeDeps: {
       include: []
